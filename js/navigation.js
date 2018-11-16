666f70c7733f19e7c7cfbe6d59e74bbdf5a00241
Veld <velddev@gmail.com>
2018-11-16 17:57:52 +0100

    Add: dashboard, general style changes

--- js/navigation.js
+++ js/navigation.js
@@ -16,22 +16,3 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
-
-window.addEventListener("scroll", () => {
-    console.log("scroll" + window.scrollY);
-    var nav = document.getElementById("global-nav-bar");
-    if(window.scrollY > 100)
-    {
-        if(!nav.classList.contains("is-fixed-top"))
-        {
-            nav.classList.add("is-fixed-top");
-        }
-    }
-    else
-    {
-        if(nav.classList.contains("is-fixed-top"))
-        {
-            nav.classList.remove("is-fixed-top");
-        }
-    }
-});

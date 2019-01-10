a78efb8157433b50450a17d7e26e243eb98a9ddb
Veld <velddev@gmail.com>
2019-01-10 15:32:36 +0100

    Added Blog API, Commands page

--- js/home.ts
+++ js/home.ts
@@ -14,3 +14,29 @@ window.onscroll = e => {
 
     previousPosition = window.scrollY;
 };
+
+// ghost blog api - this secret is CORS protected
+ghost.init({
+    clientId: "ghost-frontend",
+    clientSecret: "eb3fea17514e"
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var o = JSON.parse(this.responseText);
+        for(var post in o)
+        {
+            document.getElementById("updates").append(
+                `<div>
+                    <a href="${ghost.url}${post.slug}"
+                    <img class="image" src="${ghost.url}${post.feature_image}">
+                </div>`
+            )
+        }
+      }
+    };
+    xhttp.open("GET", ghost.url.api('posts', {limit: 3}), true);
+    xhttp.send();
+});

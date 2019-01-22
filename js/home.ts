3952ec392951a99769d05d0f118e5bbdd737e75f
Veld <velddev@gmail.com>
2019-01-23 00:14:55 +0100

    Add logo, additional extras

--- js/home.ts
+++ js/home.ts
@@ -1,15 +1,21 @@
 let previousPosition = window.scrollY;
 let header_nav = document.getElementById("global-nav-bar");
+let logo_nav = document.getElementById("logo");
 
 header_nav.classList.add("is-dark", "navbar-large", "is-fixed-top", "is-smoother");
+logo_nav.classList.add("is-large", "is-light");
 
 window.onscroll = e => {
     if (window.scrollY > 0 && previousPosition === 0) {
         header_nav.classList.remove("navbar-large");
         header_nav.classList.remove("is-dark");
+        logo_nav.classList.remove("is-light");
+        logo_nav.classList.remove("is-large");
     } else if (window.scrollY <= 0) {
         header_nav.classList.add("navbar-large");
         header_nav.classList.add("is-dark");
+        logo_nav.classList.add("is-light");
+        logo_nav.classList.add("is-large");
     }
 
     previousPosition = window.scrollY;

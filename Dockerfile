8f930646b7290add3a79add476874c127c67e638
Xetera <mythologyali@gmail.com>
2019-06-02 12:56:47 -0700

    removed old root files

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM nginx
-COPY dist /usr/share/nginx/html
-
-RUN chmod +w /etc/nginx/nginx.conf
-ADD default.conf /etc/nginx/conf.d/default.conf
--- static.js
+++
@@ -1,9 +0,0 @@
-const stc = require('node-static');
-
-const fileServer = new stc.Server('./dist');
-
-console.log("Starting server...");
-
-require('http').createServer( (request, response) =>
-	request.addListener('end', () => fileServer.serve(request, response)).resume()
-).listen(process.env.PORT || 8080, () => console.log("Server started"));
